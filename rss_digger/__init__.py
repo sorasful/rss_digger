920f6d2493534c94c63cad5d08a6dbf856020001
sorasful <teva.krief@hotmail.fr>
2025-02-04 16:03:04 +0100

    Remove the path.isabsolute to check if url is absolute, just use the scheme

--- rss_digger/__init__.py
+++ rss_digger/__init__.py
@@ -16,9 +16,6 @@ HANDLERS: list[GenericRSSHandler] = [
 async def get_rss_feeds_for(url: str) -> list[str]:
     furl_url = furl(url=url)
 
-    if not furl_url.path.isabsolute:
-        raise ValueError("URL must be absolute")
-
     if furl_url.scheme not in {"http", "https"}:
         raise ValueError("URL scheme must be http or https")
 
